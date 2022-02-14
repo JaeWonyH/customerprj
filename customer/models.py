326f65e9571896920e7f66953ea1196a1b24e387
JaeWonyH <ghkd6535@naver.com>
2022-02-14 17:19:44 +0900

    customer 정보 웹사이트

---
+++ customer/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    birthdate = models.DateField()
+    gender = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name + '('+str(self.id)+')'
