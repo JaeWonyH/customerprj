fe4943af44c22e9ff20326efc4c883ac60bc8d94
JaeWonyH <ghkd6535@naver.com>
2022-02-15 16:14:49 +0900

    customer service

--- customer/urls.py
+++ customer/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.customer_list, name='customer_list'),
+
+    path('', views.customer_list, name='customer_list'),
+    path('customer/<int:pk>', views.customer_detail, name='customer_detail'),
+
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Customer
 
 # Create your views here.
+#customer detail list
+def customer_detail(request,pk):
+    cust = get_object_or_404(Customer, pk = pk)
+    return render(request,'customer/customer_detail.html',{'customer':cust})
+
+
+#customer list
 def customer_list(request):
-    return render(request, 'customer/customer_list.html')
+    custs = Customer.objects.order_by('name')
+    return render(request, 'customer/customer_list.html',{'customers':custs})
+
+
